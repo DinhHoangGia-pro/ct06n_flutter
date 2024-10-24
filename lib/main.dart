d08a4beaa9f589ee60bb63723a85659994c4790f
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-24 21:13:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:helloworld/DemoStatefulWidget.dart';
 import 'package:helloworld/DemoStatelessWidget.dart';
+import 'package:helloworld/Home.dart';
 //import 'package:flutter/cupertino.dart';
 // import 'package:helloworld/Hienthisanpham.dart';
 // import 'package:helloworld/MyHttpOverrides.dart';
@@ -17,6 +19,9 @@ void main() {
           title: Image.network(
               'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060'),
         ),
-        body: MyStatelessWidget()),
+        body: ListView(
+          children: [MyStatelessWidget(), MyStatefulWidget()],
+        )),
   ));
+  //runApp(Home());
 }
