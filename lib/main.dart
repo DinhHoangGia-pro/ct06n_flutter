eceb88ea5107de7a61a8e9d0ed64c71a42a86600
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-24 09:08:52 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:helloworld/Hienthisanpham.dart';
-import 'package:helloworld/MyHttpOverrides.dart';
-import 'package:helloworld/Home.dart';
+import 'package:helloworld/DemoStatelessWidget.dart';
+//import 'package:flutter/cupertino.dart';
+// import 'package:helloworld/Hienthisanpham.dart';
+// import 'package:helloworld/MyHttpOverrides.dart';
+// import 'package:helloworld/Home.dart';
 
 void main() {
-  HttpOverrides.global = new MyHttpOverrides();
+  // HttpOverrides.global = new MyHttpOverrides();
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      appBar: AppBar(
-        title: Image.network(
-            'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060'),
-      ),
-      body: Scaffold(
-        appBar: AppBar(title: Text("Tiêu đề111")),
-        body: Text("Xin chào các bạn"),
-      ),
-    ),
+        appBar: AppBar(
+          title: Image.network(
+              'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060'),
+        ),
+        body: MyStatelessWidget()),
   ));
 }
