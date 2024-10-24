0d3c0f2b976b5281338eec109059ece997de993a
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-24 08:47:15 -0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:helloworld/Hienthisanpham.dart';
+import 'package:helloworld/MyHttpOverrides.dart';
+import 'package:helloworld/Home.dart';
+
+void main() {
+  HttpOverrides.global = new MyHttpOverrides();
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Image.network(
+            'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060'),
+      ),
+      body: Scaffold(
+        appBar: AppBar(title: Text("Tiêu đề111")),
+        body: Text("Xin chào các bạn"),
+      ),
+    ),
+  ));
+}
